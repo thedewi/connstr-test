118f12bc5913177b7dd3cba8c16ab787d541aad2
David Young <thedewi@users.noreply.github.com>
2023-06-12 22:33:54 +1000

    Implement connection string tester

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.SqlClient;
+
+if (args.Length == 0) {
+    Console.Error.WriteLine("Please supply a connection string.");
+    return;
+}
+
+using var db = new SqlConnection(args[0]);
+using var cmd = new SqlCommand("select 'Connected.'", db);
+db.Open();
+Console.WriteLine(cmd.ExecuteScalar());
